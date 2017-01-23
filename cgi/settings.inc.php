0cdc64ac85780814561e5518571e7e7d6a82270c
genom-by <brath@mail.ru>
2017-01-23 21:56:24 +0300

    local columns2

--- cgi/settings.inc.php
+++ cgi/settings.inc.php
@@ -4,13 +4,14 @@ define(ISSQLITE, 'true');
 //define(DSNSQLITE,'sqlite2:');
 //define(DBPATHSQLITE,'linkbox.db3');
 
-define(DBPATHSQLITE,'D:\Denwer3.5\home\localhost\www\tt\obus\assets\db\obus.db3');
-//define(DBPATHSQLITE,'D:\Denwer\home\localhost\www\tt\LinkBox\db\linkbox.db3');
+//define(DBPATHSQLITE,'D:\Denwer3.5\home\localhost\www\tt\obus\assets\db\obus.db3'); //work
+define(DBPATHSQLITE,'D:\Denwer\home\localhost\www\tt\obus\assets\db\obus.db3'); //home
 define(DSNSQLITE,'sqlite:');
 define(PAGING_NUM_PER_PAGE,'10');
 
 define(SITE_ROOT, 'tt/obus');
-define(SITE_DIR, 'D:\Denwer3.5\home\localhost\www\tt\obus');
+//define(SITE_DIR, 'D:\Denwer3.5\home\localhost\www\tt\obus'); //work
+define(SITE_DIR, 'D:\Denwer\home\localhost\www\tt\obus'); //home
 
 //define(SITE_ROOT, 'tt/LinkBox');
 //define(SITE_DIR, 'D:\Denwer\home\localhost\www\tt\LinkBox');
